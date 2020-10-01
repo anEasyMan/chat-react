2025a79dba8f304d9980b21995b4c0926b94371d
fireFlying-jg <1301501708@qq.com>
2020-10-01 10:59:20 +0800

    提交代码

--- src/pages/input/index.js
+++ src/pages/input/index.js
@@ -1,12 +1,43 @@
 import React from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
 
-import {Input} from 'antd';
-const { TextArea } = Input;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  })
+);
 
 class UserInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.classes = useStyles;
+  }
   render() {
     return (
-        <TextArea rows={4} bordered={false} />
+      <div className="input">
+        <div className="textarea">
+          <textarea className="textarea-input" />
+          <div className="btn-div">
+            <div className="div-reset">
+              <Button
+                variant="contained"
+                size="large"
+                color="primary"
+                className={this.classes.margin}
+              >
+                Large
+              </Button>
+            </div>
+            <div className="div-send">world</div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
